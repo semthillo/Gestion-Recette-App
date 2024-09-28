069ce3220e7ebefa70a027937841431328fbc9db
Ousmanly <lyousmane492@gmail.com>
2024-09-28 14:28:38 +0200

    Update Home page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <div class="page-background container mt-5">
     <div class="mb-4 d-flex justify-content-end">
       <button @click="changeLanguage('en')" class="btn btn-primary me-2">English</button>
@@ -6,47 +6,60 @@
     </div>
 
     <h1 class="animated-header mt-4 text-center">{{ $t('welcome_message') }}</h1>
-    <div class="d-flex flex-row justify-content-center">
-      <div class="card mt-5 me-5" style="width: 18rem; height: fit-content">
-        <img
-          src="/src/assets/images/poulet-yassa.jpg"
-          class="card-img-top"
-          alt="Poulet Yassa"
-          style="height: 12rem"
-        />
-        <div class="card-body">
-          <h5 class="card-title">Poulet Yassa</h5>
-          <p class="card-text">
-            {{ $t('yassa.description') }}
-          </p>
+
+    <!-- Ajouter un row Bootstrap -->
+    <div class="row mt-5 justify-content-center">
+      <!-- Colonne responsive pour la première carte -->
+      <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
+        <div class="card h-100">
+          <img
+            src="/src/assets/images/poulet-yassa.jpg"
+            class="card-img-top"
+            alt="Poulet Yassa"
+            style="height: 12rem"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Poulet Yassa</h5>
+            <p class="card-text">
+              {{ $t('yassa.description') }}
+            </p>
+          </div>
         </div>
       </div>
-      <div class="card mt-5 me-5" style="width: 18rem; height: fit-content">
-        <img
-          src="/src/assets/images/niébé.jpg"
-          class="card-img-top"
-          alt="Niébé"
-          style="height: 12rem"
-        />
-        <div class="card-body">
-          <h5 class="card-title">Niébé</h5>
-          <p class="card-text">
-            {{ $t('niebe.description') }}
-          </p>
+
+      <!-- Colonne responsive pour la deuxième carte -->
+      <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
+        <div class="card h-100">
+          <img
+            src="/src/assets/images/niébé.jpg"
+            class="card-img-top"
+            alt="Niébé"
+            style="height: 12rem"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Niébé</h5>
+            <p class="card-text">
+              {{ $t('niebe.description') }}
+            </p>
+          </div>
         </div>
       </div>
-      <div class="card mt-5" style="width: 18rem; height: fit-content">
-        <img
-          src="/src/assets/images/Riz.jpg"
-          class="card-img-top"
-          alt="Riz au Poisson"
-          style="height: 12rem"
-        />
-        <div class="card-body">
-          <h5 class="card-title">Riz au Poisson</h5>
-          <p class="card-text">
-            {{ $t('rice.description') }}
-          </p>
+
+      <!-- Colonne responsive pour la troisième carte -->
+      <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
+        <div class="card h-100">
+          <img
+            src="/src/assets/images/Riz.jpg"
+            class="card-img-top"
+            alt="Riz au Poisson"
+            style="height: 12rem"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Riz au Poisson</h5>
+            <p class="card-text">
+              {{ $t('rice.description') }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -79,98 +92,6 @@ const changeLanguage = (locale) => {
     transform: translateY(0);
   }
 }
-.animated-header {
-  animation: fadeInUp 1s ease-out;
-}
-</style> -->
-<template>
-  <div class="page-background container mt-5">
-    <h1 class="animated-header mt-4 text-center">
-      Welcome you're at a good place
-    </h1>
-    <div class="row justify-content-center">
-      <div class="col-lg-4 col-md-6 col-sm-12">
-        <div class="card mt-5" style="height: fit-content;">
-          <img
-            src="/src/assets/images/poulet-yassa.jpg"
-            class="card-img-top"
-            alt="Poulet Yassa"
-            style="height: 12rem"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Poulet Yassa</h5>
-            <p class="card-text">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas,
-              vitae quaerat commodi illum earum aspernatur aut, doloremque beatae
-              omnis quisquam id quidem aliquam.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6 col-sm-12">
-        <div class="card mt-5" style="height: fit-content;">
-          <img
-            src="/src/assets/images/niébé.jpg"
-            class="card-img-top"
-            alt="Niébé"
-            style="height: 12rem"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Niébé</h5>
-            <p class="card-text">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas,
-              vitae quaerat commodi illum earum aspernatur aut, doloremque beatae
-              omnis quisquam id quidem aliquam.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6 col-sm-12">
-        <div class="card mt-5" style="height: fit-content;">
-          <img
-            src="/src/assets/images/Riz.jpg"
-            class="card-img-top"
-            alt="Riz au Poisson"
-            style="height: 12rem"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Riz au Poisson</h5>
-            <p class="card-text">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas,
-              vitae quaerat commodi illum earum aspernatur aut, doloremque beatae
-              omnis quisquam id quidem aliquam.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
-<script setup>
-</script>
-
-<style scoped>
-.page-background {
-  background-color: #f0f0f0;
-  min-height: 100vh;
-  color: #333;
-  padding: 20px;
-}
-
-@keyframes fadeInUp {
-  from {
-    opacity: 0;
-    transform: translateY(20px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
-
 .animated-header {
   animation: fadeInUp 1s ease-out;
 }
